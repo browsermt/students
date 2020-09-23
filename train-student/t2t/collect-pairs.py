9f35852ea63726c99f4e2307ce51eed75a4d1fa7
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-09-23 06:14:19 +0000

    temporary commit; shifting to local

---
+++ train-student/t2t/collect-pairs.py
@@ -0,0 +1,14 @@
+from argparse import ArgumentParser
+import warnings
+
+
+def main(args):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser('Utility to generate parallel data from T2T translation outputs') 
+    parser.add_argument('--t2t-output', type=str, required=True)
+    parsr.add_argument
