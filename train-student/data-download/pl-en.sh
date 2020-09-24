3441d5b5a3847ab3b0dd2819358a9907f4bc6088
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-09-24 20:17:13 +0530

    adding a data-download script for csd3

---
+++ train-student/data-download/pl-en.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# 
+# Usage:
+# ./pl-en.sh DOWNLOAD_DIR
+# Downloads Polish-English models into a dir
+
+DOWNLOAD_DIR="$1"
+
+PARALLEL=(
+    "https://s3.amazonaws.com/web-language-models/paracrawl/release5.1/en-pl.txt.gz" # Paracrawl
+    "https://tilde-model.s3-eu-west-1.amazonaws.com/rapid2019.en-pl.tmx.zip" 
+    "http://data.statmt.org/wikititles/v2/wikititles-v2.pl-en.tsv.gz"
+    "http://www.statmt.org/europarl/v10/training/europarl-v10.pl-en.tsv.gz"
+
+)
+
+MONOLINGUAL=(
+    "http://www.statmt.org/europarl/v10/training-monolingual/europarl-v10.pl.tsv.gz"
+    "http://data.statmt.org/news-crawl/pl/news.2019.pl.shuffled.deduped.gz"
+
+)
+
+PARALLEL_DIR="$DOWNLOAD_DIR/parallel"
+mkdir -p $PARALLEL_DIR
+
+for parallel in ${PARALLEL[@]}; do
+    echo "Downloading $parallel"
+    wget --continue \
+        --directory-prefix="$PARALLEL_DIR" \
+        "$parallel"
+done
+
+MONOLINGUAL_DIR="$DOWNLOAD_DIR/monolingual"
+mkdir -p $MONOLINGUAL_DIR
+
+MONOLINGUAL_DIR="$DOWNLOAD_DIR/monolingual"
+for monolingual in ${MONOLINGUAL[@]}; do
+    echo "Downloading $monolingual"
+    wget --continue \
+        --directory-prefix="$MONOLINGUAL_DIR" \
+        "$monolingual"
+done
