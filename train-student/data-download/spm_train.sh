64b0cdc75d54c4b72974271f6f5db3978684dec8
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-09-28 10:59:30 +0530

    train file init right; --train_x-large-corpus

--- train-student/data-download/spm_train.sh
+++ train-student/data-download/spm_train.sh
@@ -30,6 +30,9 @@ mkdir -p $LOCAL_DATA
 OUTPUT_DIR="${DATA_DIR}/sentencepiece-models"
 mkdir -p $OUTPUT_DIR
 
+rm -v "${LOCAL_DATA}/train.${SRC}"
+rm -v "${LOCAL_DATA}/train.${TGT}"
+
 find "$DATA_DIR" -iname "*.${SRC}" | xargs -I% cat % >> "${LOCAL_DATA}/train.${SRC}"
 find "$DATA_DIR" -iname "*.${TGT}" | xargs -I% cat % >> "${LOCAL_DATA}/train.${TGT}"
 
@@ -42,6 +45,8 @@ function spm_train_lang {
         --character_coverage 1.0
         --vocab_size "$VOCAB"
         --num_threads "${SLURM_CPUS_PER_TASK}"
+        --train_extremely_large_corpus
+
     )
 
     spm_train "${SPM_ARGS[@]}"
