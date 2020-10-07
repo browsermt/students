9e0be5aaaafcb396ed4659568b245d9b6167302a
Jaume Zaragoza <jzaragoza@prompsit.com>
2020-10-07 16:30:23 +0200

    Add Norwegian Bokmal

---
+++ nben/download-models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for pair in nben; do
+    wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/$pair.teacher.base/model.npz
+    wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/vocab.nben.spm
+
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.npz
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.bin
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.alphas.bin
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/lex.s2t.gz
+done
