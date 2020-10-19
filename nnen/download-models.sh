937872652e869c531511b3126c48bea44e883f35
Jaume Zaragoza <jzaragoza@prompsit.com>
2020-10-19 14:07:19 +0200

    Add Norwegian Nynorsk

---
+++ nnen/download-models.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for pair in nnen; do
+    wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/$pair.teacher.base/model.npz
+    wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/vocab.$pair.spm
+
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.npz
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.bin
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.alphas.bin
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/lex.s2t.gz
+    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/vocab.$pair.spm
+done
