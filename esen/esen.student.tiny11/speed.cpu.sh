d153932011cb576a2bfb58290e67c455c7742f83
Motin <motin@motin.eu>
2020-12-06 22:25:22 +0200

    Temporarily set mini/maxi batch sizes to 1/1 in esen speed benchmark script

--- esen/esen.student.tiny11/speed.cpu.sh
+++ esen/esen.student.tiny11/speed.cpu.sh
@@ -14,7 +14,7 @@ echo "### Translating wmt13 $SRC-$TRG on CPU"
 $MARIAN/marian-decoder $@ \
     --relative-paths -m model.npz -v vocab.$SRC$TRG.spm vocab.$SRC$TRG.spm \
     -i speed/newstest2013.$SRC.top10lines -o speed/cpu.newstest2013.$TRG \
-    --beam-size 1 --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 128 \
+    --beam-size 1 --mini-batch 1 --maxi-batch 1 --maxi-batch-sort src -w 128 \
     --skip-cost --cpu-threads 1 \
     --quiet --quiet-translation --log speed/cpu.newstest2013.log
 
