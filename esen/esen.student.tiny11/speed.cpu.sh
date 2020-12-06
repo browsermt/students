19685dc9588c87d3317a0efd046fcc43e8541d28
Motin <motin@motin.eu>
2020-12-06 10:34:19 +0200

    Temporarily remove shortlist arg for esen speed benchmark script

--- esen/esen.student.tiny11/speed.cpu.sh
+++ esen/esen.student.tiny11/speed.cpu.sh
@@ -14,8 +14,10 @@ $MARIAN/marian-decoder $@ \
     --relative-paths -m model.npz -v vocab.$SRC$TRG.spm vocab.$SRC$TRG.spm \
     -i speed/newstest2013.$SRC -o speed/cpu.newstest2013.$TRG \
     --beam-size 1 --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 128 \
-    --skip-cost --shortlist lex.s2t.gz 50 50 --cpu-threads 1 \
+    --skip-cost --cpu-threads 1 \
     --quiet --quiet-translation --log speed/cpu.newstest2013.log
+
+#--shortlist lex.s2t.gz 50 50
 
 tail -n1 speed/cpu.newstest2013.log
 sacrebleu -t wmt13 -l $SRC-$TRG < speed/cpu.newstest2013.$TRG | tee speed/cpu.newstest2013.$TRG.bleu
