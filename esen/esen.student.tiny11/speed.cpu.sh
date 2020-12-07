e03815cc3f4278583e27caeea19472a9bd4a37c7
Motin <motin@motin.eu>
2020-12-07 15:21:06 +0200

    Add 100 and 300 top lines versions of newstest2013.es

--- esen/esen.student.tiny11/speed.cpu.sh
+++ esen/esen.student.tiny11/speed.cpu.sh
@@ -12,11 +12,13 @@ mkdir -p speed
 
 sacrebleu -t wmt13 -l $SRC-$TRG --echo src > speed/newstest2013.$SRC
 head -n10 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top10lines
+head -n100 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top100lines
+head -n300 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top300lines
 
 echo "### Translating wmt13 $SRC-$TRG on CPU"
 $MARIAN/marian-decoder $@ \
     --relative-paths -m model.npz -v vocab.$SRC$TRG.spm vocab.$SRC$TRG.spm \
-    -i speed/newstest2013.$SRC.top10lines -o speed/cpu.newstest2013.$TRG \
+    -i speed/newstest2013.$SRC.top100lines -o speed/cpu.newstest2013.$TRG \
     --beam-size 1 --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 128 \
     --skip-cost --shortlist lex.s2t 50 50 --cpu-threads 1 \
     --quiet --log speed/cpu.newstest2013.log
