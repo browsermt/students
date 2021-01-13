108e4eb36afcd60276bc06a6335ff271d1695cb5
Motin <motin@motin.eu>
2021-01-13 11:03:55 +0200

    Update intgemm8bit and alphas benchmark scripts to match wasm benchmarks

--- esen/esen.student.tiny11/speed.cpu.intgemm8bit.sh
+++ esen/esen.student.tiny11/speed.cpu.intgemm8bit.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-MARIAN=/home/nbogoych/marian-dev/build_VSCODE
+# Prevent OneDNN's attempts to parallelize the GEMMs over all threads which is not efficient for the small gemms that we have
+export OMP_NUM_THREADS=1
+
+MARIAN=../../marian-dev/build
 
 SRC=es
 TRG=en
 
 mkdir -p speed
 
-~/.local/bin/sacrebleu -t wmt13 -l $SRC-$TRG --echo src > speed/newstest2013.$SRC
+sacrebleu -t wmt13 -l $SRC-$TRG --echo src > speed/newstest2013.$SRC
+head -n10 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top10lines
+head -n100 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top100lines
+head -n300 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top300lines
 
 test -e model.intgemm.bin || $MARIAN/marian-conv -f model.npz -t model.intgemm.bin --gemm-type intgemm8
 
 echo "### Translating wmt18 $SRC-$TRG on CPU"
 $MARIAN/marian-decoder $@ \
-    --relative-paths -m model.intgemm.bin -v vocab.esen.spm vocab.esen.spm \
-    -i speed/newstest2013.$SRC -o speed/cpu.newstest2013.$TRG \
+    --relative-paths -m model.npz -v vocab.esen.spm vocab.esen.spm \
+    -i speed/newstest2013.$SRC.top300lines -o speed/cpu.newstest2013.$TRG \
     --beam-size 1 --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 128 \
-    --skip-cost --shortlist lex.s2t.gz 50 50 --cpu-threads 1 \
+    --skip-cost --shortlist lex.s2t 50 50 --cpu-threads 1 \
     --quiet --quiet-translation --log speed/cpu.newstest2013.log --int8shift
 
 tail -n1 speed/cpu.newstest2013.log
-~/.local/bin/sacrebleu -t wmt13 -l $SRC-$TRG < speed/cpu.newstest2013.$TRG | tee speed/cpu.newstest2013.$TRG.bleu
+# Needs the entire input to be useful:
+# sacrebleu -t wmt13 -l $SRC-$TRG < speed/cpu.newstest2013.$TRG | tee speed/cpu.newstest2013.$TRG.bleu
--- esen/esen.student.tiny11/speed.cpu.intgemm8bitalpha.sh
+++ esen/esen.student.tiny11/speed.cpu.intgemm8bitalpha.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-MARIAN=/home/nbogoych/marian-dev/build_VSCODE
+# Prevent OneDNN's attempts to parallelize the GEMMs over all threads which is not efficient for the small gemms that we have
+export OMP_NUM_THREADS=1
+
+MARIAN=../../marian-dev/build
 
 SRC=es
 TRG=en
 
 mkdir -p speed
 
-~/.local/bin/sacrebleu -t wmt13 -l $SRC-$TRG --echo src > speed/newstest2013.$SRC
+sacrebleu -t wmt13 -l $SRC-$TRG --echo src > speed/newstest2013.$SRC
+head -n10 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top10lines
+head -n100 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top100lines
+head -n300 speed/newstest2013.$SRC > speed/newstest2013.$SRC.top300lines
 
 if [ ! -f model.intgemm.alphas.bin ]; then
 
@@ -26,11 +32,12 @@ fi
 
 echo "### Translating wmt13 $SRC-$TRG on CPU"
 $MARIAN/marian-decoder $@ \
-    --relative-paths -m model.intgemm.alphas.bin -v vocab.esen.spm vocab.esen.spm \
-    -i speed/newstest2013.$SRC -o speed/cpu.newstest2013.$TRG \
+    --relative-paths -m model.intgemm.alphas.bin -v vocab.$SRC$TRG.spm vocab.$SRC$TRG.spm \
+    -i speed/newstest2013.$SRC.top300lines -o speed/cpu.newstest2013.$TRG \
     --beam-size 1 --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 128 \
-    --skip-cost --shortlist lex.s2t.gz 50 50 --cpu-threads 1 \
+    --skip-cost --shortlist lex.s2t 50 50 --cpu-threads 1 \
     --quiet --quiet-translation --log speed/cpu.newstest2013.log --int8shiftAlphaAll
 
 tail -n1 speed/cpu.newstest2013.log
-~/.local/bin/sacrebleu -t wmt13 -l $SRC-$TRG < speed/cpu.newstest2013.$TRG | tee speed/cpu.newstest2013.$TRG.bleu
+# Needs the entire input to be useful:
+# sacrebleu -t wmt13 -l $SRC-$TRG < speed/cpu.newstest2013.$TRG | tee speed/cpu.newstest2013.$TRG.bleu
