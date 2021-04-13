49e497b43896d6a9cacd819794dd0d1646cfbdb3
Qianqian Zhu <qianqian.zhu@hotmail.com>
2021-04-13 10:52:55 +0100

    rename files in server side

--- csen/download-models.sh
+++ csen/download-models.sh
@@ -6,19 +6,13 @@ do
 	do
 		mkdir -p $lang.student.$model
 		cd $lang.student.$model
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.8bit-finetuned.npz
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.8bit-finetuned.alphas.npz
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.8bit-finetuned.intgemm8.bin
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.8bit-finetuned.intgemm8.alphas.bin
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.npz
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.alphas.npz
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.intgemm.bin
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.intgemm.alphas.bin
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.gz
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.bin
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.spm
-        # rename files for consistency
-		mv model.8bit-finetuned.npz model.npz
-        mv model.8bit-finetuned.alphas.npz model.alphas.npz
-		mv model.8bit-finetuned.intgemm8.bin model.intgemm.bin
-		mv model.8bit-finetuned.intgemm8.alphas.bin model.intgemm.alphas.bin
-		mv vocab.spm vocab.csen.spm
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.csen.spm
 		cd ..
 	done
 done
@@ -33,9 +27,7 @@ do
 		cd $lang.student.$model
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.npz.best-bleu-detok.npz
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.gz
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.spm
-		# rename files for consistency
-		mv vocab.spm vocab.csen.spm
+		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.csen.spm
 		cd ..
 	done
 done
