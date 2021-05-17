27ae32ff1e0fb14169fda7e879fc592e93b8e112
Qianqian Zhu <qianqian.zhu@hotmail.com>
2021-05-17 12:07:29 +0100

    now lex.s2t.gz can be removed from download scripts safely

--- csen/download-models.sh
+++ csen/download-models.sh
@@ -10,7 +10,6 @@ do
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.alphas.npz
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.intgemm.bin
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.intgemm.alphas.bin
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.gz
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.bin
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.csen.spm
 		cd ..
@@ -26,7 +25,6 @@ do
 		mkdir -p $lang.student.$model
 		cd $lang.student.$model
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/model.npz.best-bleu-detok.npz
-		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/lex.s2t.gz
 		wget -nc http://data.statmt.org/bergamot/models/8bit-students/$lang/$model/vocab.csen.spm
 		cd ..
 	done
--- nben/download-models.sh
+++ nben/download-models.sh
@@ -7,7 +7,6 @@ for pair in nben; do
     wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.npz
     wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.bin
     wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.alphas.bin
-    wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/lex.s2t.gz
     wget -nc -P $pair.student.tiny11/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/lex.s2t.bin
     cp $pair.teacher.base/vocab.nben.spm $pair.student.tiny11/
 done
