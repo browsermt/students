64d0bfa05c75ffdd814475f60730c8a58cd267c7
Nikolay Bogoychev <nheart@gmail.com>
2022-06-23 16:08:36 +0100

    tar fixes

--- deploy.students.sh
+++ deploy.students.sh
@@ -25,7 +25,7 @@ for language in cs de es et is nb nn bg pl fr; do
         cd $student_model
         echo "Deploying $student_model"
         rm -rf $student_model.tar.gz # Remove old model if it exists
-        tar -czvf --owner=0 --group=0 $student_model.tar.gz --transform "s,^,${student_model}/," config.intgemm8bitalpha.yml model.intgemm.alphas.bin speed.cpu.intgemm8bitalpha.sh lex.s2t.bin vocab.$dir.spm catalog-entry.yml model_info.json
+        tar -czvf $student_model.tar.gz --transform "s,^,${student_model}/," config.intgemm8bitalpha.yml model.intgemm.alphas.bin speed.cpu.intgemm8bitalpha.sh lex.s2t.bin vocab.$dir.spm catalog-entry.yml model_info.json --owner=0 --group=0
         scp $student_model.tar.gz $USER@lofn:/mnt/vali0/www/data.statmt.org/bergamot/models/$dir
         cd ..
         ../generate_models_json.py ../models.json $student_model/$student_model.tar.gz $student_model $dir $URLBASE
