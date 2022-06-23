d4f7af26a3346ff4663fa4a44fcdf6a27b780fc7
Nikolay Bogoychev <nheart@gmail.com>
2022-06-23 16:05:40 +0100

    Some more fixes

--- fren/download-models.sh
+++ fren/download-models.sh
@@ -6,27 +6,27 @@ do
 	do
 		mkdir -p $lang.student.$model
 		cd $lang.student.$model
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.npz
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.npz.decoder.yml
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.alphas.npz
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.intgemm.bin
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.intgemm.alphas.bin
-                wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/lex.s2t.bin
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/lex.s2t.pruned.gz
-		wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/vocab.bgen.spm
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.npz
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.npz.decoder.yml
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.alphas.npz
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.intgemm.bin
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.intgemm.alphas.bin
+                wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/lex.s2t.bin
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/lex.s2t.pruned.gz
+		wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/vocab.fren.spm
 		cd ..
 	done
 done
 
-for lang in bgen enbg
+for lang in fren enfr
 do
         for model in teacher
         do
                 mkdir -p $lang.$model
                 cd $lang.$model
-                wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.npz
-                wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/model.npz.decoder.yml
-                wget -nc http://data.statmt.org/bergamot/models/bgen/$lang/$model/vocab.bgen.spm
+                wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.npz
+                wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/model.npz.decoder.yml
+                wget -nc http://data.statmt.org/bergamot/models/fren/$lang/$model/vocab.fren.spm
                 cd ..
         done
 done
