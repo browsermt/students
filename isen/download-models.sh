628ac101f64190788b85e99c3047e73ccca0dd03
ZJaume <jzaragoza@prompsit.com>
2023-05-29 13:52:37 +0000

    Remove old teacher Icelandic from download

--- isen/download-models.sh
+++ isen/download-models.sh
@@ -2,14 +2,9 @@
 
 for pair in isen; do
     for model in base tiny11; do
-        wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/$pair.teacher.base/model.npz
-        wget -nc -P $pair.teacher.base/ http://data.statmt.org/bergamot/models/$pair/vocab.$pair.spm
-
         wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/model.npz
-        wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/model.intgemm.bin
         wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/model.intgemm.alphas.bin
-        wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/lex.s2t.gz
         wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/lex.s2t.bin
-        wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/vocab.$pair.spm
+        wget -nc -P $pair.student.$model/ http://data.statmt.org/bergamot/models/$pair/$pair.student.$model/vocab.$pair.spm
     done
 done
