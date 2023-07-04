7af2be548b54502f587924b24b6be843aaec5d37
ZJaume <jzaragoza@prompsit.com>
2023-07-04 15:18:16 +0000

    Add Catalan-English model

---
+++ caen/caen.student.tiny11/speed.cpu.intgemm8bitalpha.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -v
+
+MARIAN=../../../students/marian-bergamot/build
+
+SRC=ca
+TRG=en
+
+mkdir -p speed
+
+# Custom test sets.
+DATA=../data
+
+for prefix in flores200test; do
+   echo "### Translating $prefix $SRC-$TRG"
+    cat $DATA/$prefix.$SRC \
+        | tee speed/$prefix.$SRC \
+        | $MARIAN/marian-decoder -c config.intgemm8bitalpha.yml --quiet --log speed/$prefix.log --cpu-threads 1 \
+        | tee speed/$prefix.$TRG \
+        | sacrebleu $DATA/$prefix.$TRG \
+        | tee speed/$prefix.$TRG.bleu
+    tail -n1 speed/$prefix.log
+done
